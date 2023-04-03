2bf57d546540edc0622c0a16682ac4c84c9b10f4
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-04-03 15:41:44 +0200

    (feature): Add ShowTodos component

---
+++ todo-vue/src/components/ShowTodos.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Todo } from "../models/Todo";
+
+const todos = ref<Todo[]>([]);
+
+const userInput = ref("");
+
+const toggleTodoDone = (i: number) => {
+  todos.value[i].done = !todos.value[i].done;
+};
+
+const addTodo = () => {
+  todos.value.push(new Todo(userInput.value, false));
+  userInput.value = "";
+};
+
+const removeTodo = (i: number) => {
+  todos.value.splice(i, 1);
+};
+</script>
+
+<template></template>
+
+<style scoped></style>
